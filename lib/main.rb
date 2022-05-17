902540a3a86811372f5048fe53c2ab08a1945365
guillermo-delprado <galfonso.delprado@gmail.com>
2022-05-17 16:29:52 -0500

    Adding some functions to test the program
    Co-authored-by:@jumaquba
    
    Co-authored-by:@jumaquba <jumaquba@gmail.com>
    Co-authored-by:@V-Juarez <mytacam78@gmail.com>
    Co-authored-by:@matias.devjam

--- lib/main.rb
+++ lib/main.rb
@@ -4,9 +4,20 @@
 class Bowling
   def intialize(_roll)
     @roll = 0
+    @total_rolls = Array.new(20,0)
   end
 
-  def frame_points; end
+  def tiro 
+    @total_rolls = 2.times { |roll| rand(0...10) }
+  end
+
+  def frame_points
+    frame_score = 0
+  end
 
   def global_points; end
 end
+
+test = Bowling.new
+
+puts test.tiro.inspect
